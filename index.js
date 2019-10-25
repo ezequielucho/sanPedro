8d97c60e70b2c495cbbfe5830511810f81fd16e7
Ezequiel <47889917+ezequielucho@users.noreply.github.com>
2019-10-25 13:15:05 +0200

    mssql connect

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 var express = require('express');
+var sql = require('mssql');
 var path = require('path');
 var app = express();
 
+async function testSql()
+{
+    try
+    {
+        await sql.ConnectionError('mssql:sa:LOperas93786@silema.hiterp.com/Fac_Demo');
+        var result = await sql.query`select codi, nom from clients`;
+        return result;
+    } catch(err)
+    {
+        console.log(err);
+    }
+}
+
 app.get("/", function(req, res){
-    res.writeHead(200, {'content-type': 'text/plain'})
-    res.end('Hola mundo');
+    testSql().then(resultado=>{
+        if(resultado)
+        {
+            res.writeHead(200, {'content-type': 'application/json'})
+            res.end(JSON.stringify(resultado));
+        }
+        else
+        {
+            console.log("Error 10");
+            res.writeHead(200, {'content-type': 'text/html'})
+            res.end('Error en la consulta procedimiento');
+        }
+    });
 });
 
-app.listen(80);
+app.listen(8080);
