c540b02535e730434f06d65c390d91d51399c91a
Eze <ezequielucho@gmail.com>
2019-10-27 15:36:28 +0100

    WebSockets Test 1

--- index.js
+++ index.js
@@ -1,39 +1,12 @@
-var express = require('express');
-var app = express();
+const path = require('path');
+const express = require('express');
+const app = express();
 
-app.get('/', function (req, res) {
-   
-    var sql = require("mssql");
+app.set('port', process.env.PORT || 8080);
 
-    // config for your database
-    var config = {
-        user: 'sa',
-        password: 'LOperas93786',
-        server: 'silema.hiterp.com', 
-        database: 'Fac_Demo' 
-    };
+app.use(express.static(path.join(__dirname, 'public')));
 
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
 
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select codi, nom from clients', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response            
-            res.writeHead(200, {'Content-Type': 'application/javascript'})
-            res.end(JSON.stringify(recordset.recordsets));
-            sql.close();
-        });
-    });
-});
-
-var server = app.listen(8080, function () {
-    console.log('Server is running..');
-});
+app.listen(app.get('port'), () =>{
+    console.log('server on port', app.get('port'));
+})
