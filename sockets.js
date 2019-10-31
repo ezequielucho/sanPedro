f3d8b0446e7b96fac17c57c18f594b00bded9bfb
Ezequiel <47889917+ezequielucho@users.noreply.github.com>
2019-10-31 15:00:22 +0100

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -12,7 +12,7 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
             if(data.password == 'LOperas93786')
             {
                 conexion.recHit('Hit', `SELECT ll.Llicencia, ll.Empresa, ll.LastAccess, we.Db FROM llicencies ll LEFT JOIN Web_Empreses we ON ll.Empresa = we.Nom WHERE ll.Llicencia = ${data.numLicencia}`).then(function(data){
-                    if(data.recordset === 1)
+                    if(data.recordset.length === 1)
                     {
                         conexion.recHit(data.recordset[0].Db, 'SELECT * FROM Clients').then((res)=>{
                             socket.emit('test', res.recordset);
