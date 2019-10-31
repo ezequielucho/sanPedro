1558447883172008a802954f6d0afbfb949a50f9
Ezequiel <47889917+ezequielucho@users.noreply.github.com>
2019-10-31 14:19:02 +0100

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -3,21 +3,24 @@ function loadSockets(io, conexion)
 {
     io.on('connection', (socket)=>
     {    
+        /* TEST */
         socket.on('eze-test', (data)=>{
             conexion.recHit('Fac_Demo', 'SELECT * FROM Clients');
         });
-
+        /* COMPROBAR E INSTALAR LICENCIA */
         socket.on('install-licencia', (data)=>{
             if(data.password == 'LOperas93786')
             {
                 conexion.recHit('Hit', `SELECT * FROM llicencies WHERE Llicencia = ${data.numLicencia}`).then(function(data){
-                    console.log(data);
+                    console.log(data.recordsets);
                 });
                 
             }
             console.log(data.numLicencia, data.password);
             //conexion.recHit('Fac_Demo', '', io);
         });
+
+        /* OTRA */
     });
 }
 module.exports.loadSockets = loadSockets;
