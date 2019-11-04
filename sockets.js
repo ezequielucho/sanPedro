f886ae9d562ad66062badae0e877a4597c84d164
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2019-11-05 00:14:30 +0100

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -50,6 +50,7 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
         socket.on('cargar-ultimo-teclado', (data)=>{
             console.log("Recibo petición ", data);
             conexion.recHit(data.database, 'SELECT Codi as id, NOM as nombre, PREU as precioConIva, TipoIva as tipoIva FROM Article').then(res2=>{
+                console.log("articulos son: ", res2)
                 conexion.recHit(data.database, `SELECT DISTINCT Ambient as nomMenu FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then(res1=>{
                     if(res1)
                     {
