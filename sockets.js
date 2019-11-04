51b153b14f72e26499283ffa20bbf5f9b19e2146
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2019-11-05 00:17:37 +0100

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -50,33 +50,39 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
         socket.on('cargar-ultimo-teclado', (data)=>{
             console.log("Recibo petición ", data);
             conexion.recHit(data.database, 'SELECT Codi as id, NOM as nombre, PREU as precioConIva, TipoIva as tipoIva FROM Article').then(res2=>{
-                console.log("articulos son: ", res2)
-                conexion.recHit(data.database, `SELECT DISTINCT Ambient as nomMenu FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then(res1=>{
-                    if(res1)
-                    {
-                        conexion.recHit(data.database, `SELECT Data, Ambient as nomMenu, article as idArticle, pos, color FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then((res)=>{
-                            if(res)
-                            {
-                                let auxObject = {
-                                    error: false,
-                                    menus: res1.recordset,
-                                    teclas: res.recordset,
-                                    articulos: res2.recordset
-                                };
-                                console.log("Voy a emitir esto: ", auxObject);
-                                socket.emit('cargar-ultimo-teclado', auxObject);
-                            }
-                            else
-                            {
-                                socket.emit('cargar-ultimo-teclado', {error: true, infoError: "Error en la respuesta de la consulta SQL"});
-                            }
-                        });
-                    }
-                    else
-                    {
-                        socket.emit('cargar-ultimo-teclado', {error: true, infoError: "Error en la respuesta de la consulta SQL 1"});
-                    }
-                });
+                try
+                {
+                    conexion.recHit(data.database, `SELECT DISTINCT Ambient as nomMenu FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then(res1=>{
+                        if(res1)
+                        {
+                            conexion.recHit(data.database, `SELECT Data, Ambient as nomMenu, article as idArticle, pos, color FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then((res)=>{
+                                if(res)
+                                {
+                                    let auxObject = {
+                                        error: false,
+                                        menus: res1.recordset,
+                                        teclas: res.recordset,
+                                        articulos: res2.recordset
+                                    };
+                                    console.log("Voy a emitir esto: ", auxObject);
+                                    socket.emit('cargar-ultimo-teclado', auxObject);
+                                }
+                                else
+                                {
+                                    socket.emit('cargar-ultimo-teclado', {error: true, infoError: "Error en la respuesta de la consulta SQL"});
+                                }
+                            });
+                        }
+                        else
+                        {
+                            socket.emit('cargar-ultimo-teclado', {error: true, infoError: "Error en la respuesta de la consulta SQL 1"});
+                        }
+                    });
+                }
+                catch(err)
+                {
+                    console.log("Error: "+err);
+                }
             });
         });
     });
