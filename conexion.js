f08c7ecc5c9b416eeefc9fc04ee57df8a668891e
Eze <ezequielucho@gmail.com>
2019-11-12 21:05:06 +0100

    Nodemon fuera
    No se utliza

--- conexion.js
+++ conexion.js
@@ -1,32 +1,4 @@
 var sql = require("mssql");
-/*
-function recHit(database, consultaSQL, io)
-{   
-    var config = {
-        user: 'sa',
-        password: 'LOperas93786',
-        server: 'silema.hiterp.com',
-        database: database
-    };
-    sql.connect(config, (err)=>
-    {
-        if(err)
-        {
-            console.log(err);
-        }
-        var request = new sql.Request();
-        request.query(consultaSQL, (err, recordset)=>
-        {
-            if(err)
-            {
-                console.log(err);
-            }
-            sql.close();
-            io.sockets.emit('escucho', JSON.stringify(recordset));
-        });
-    });
-}
-*/
 
 async function recHitBuena(database, consultaSQL) /*¡DEVUELVE UNA PROMESA!*/
 {
