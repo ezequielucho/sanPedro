09827e143c9b71bb5bcccbaba8dc8a4c9077ba82
Eze <ezequielucho@gmail.com>
2019-11-19 16:37:18 +0100

    update

--- sockets.js
+++ sockets.js
@@ -1,63 +1,55 @@
 
 function loadSockets(io, conexion) // Se devuelve data.recordset !!!
 {
-    io.on('connection', (socket)=>
-    {    
+    io.on('connection', (socket) => {
         /* TEST */
-        socket.on('eze-test', (data)=>{
-            conexion.recHit('Fac_Demo', 'SELECT * FROM Clients').then(res=>{
+        socket.on('eze-test', (data) => {
+            conexion.recHit('Fac_Demo', 'SELECT * FROM Clients').then(res => {
                 console.log(res);
             });
         });
         /* COMPROBAR E INSTALAR LICENCIA */
-        socket.on('install-licencia', (data)=>{
-            if(data.password == 'LOperas93786')
-            {
-                conexion.recHit('Hit', `SELECT ll.Llicencia, ll.Empresa, ll.LastAccess, we.Db FROM llicencies ll LEFT JOIN Web_Empreses we ON ll.Empresa = we.Nom WHERE ll.Llicencia = ${data.numLicencia}`).then(function(data){
-                    if(data.recordset.length === 1)
-                    {
+        socket.on('install-licencia', (data) => {
+            if (data.password == 'LOperas93786') {
+                conexion.recHit('Hit', `SELECT ll.Llicencia, ll.Empresa, ll.LastAccess, we.Db FROM llicencies ll LEFT JOIN Web_Empreses we ON ll.Empresa = we.Nom WHERE ll.Llicencia = ${data.numLicencia}`).then(function (data) {
+                    if (data.recordset.length === 1) {
                         /*
                         conexion.recHit(data.recordset[0].Db, 'SELECT * FROM Clients').then((res)=>{
                             socket.emit('test', res.recordset);
                         });
                         */
-                       socket.emit('install-licencia', {
-                           licencia: parseInt(data.recordset[0].Llicencia), 
-                           nombreEmpresa: data.recordset[0].Empresa,
-                           database: data.recordset[0].Db,
-                           error: false
+                        socket.emit('install-licencia', {
+                            licencia: parseInt(data.recordset[0].Llicencia),
+                            nombreEmpresa: data.recordset[0].Empresa,
+                            database: data.recordset[0].Db,
+                            error: false
                         });
                     }
-                    else
-                    {
+                    else {
                         socket.emit('install-licencia', {
                             error: true,
                             infoError: "No hay UN resultado con estos datos"
-                         });
+                        });
                     }
                 });
             }
-            else
-            {
+            else {
                 socket.emit('install-licencia', {
                     error: true,
                     infoError: "Contraseña incorrecta"
-                 });
+                });
             }
         });
 
         /* OTRA */
-        socket.on('cargar-todo', (data)=>{
-            conexion.recHit(data.database, 'SELECT Codi as id, NOM as nombre, PREU as precioConIva, TipoIva as tipoIva, EsSumable as aPeso FROM Articles').then(res2=>{
-                conexion.recHit(data.database, `SELECT DISTINCT Ambient as nomMenu FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then(res1=>{
-                    if(res1)
-                    {
-                        conexion.recHit(data.database, `SELECT Data, Ambient as nomMenu, article as idArticle, pos, color FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then((res)=>{
-                            if(res)
-                            {
-                                conexion.recHit(data.database, 'select Codi as idTrabajador, nom as nombre, memo as nombreCorto from dependentes').then(res3=>{
-                                    if(res3)
-                                    {
+        socket.on('cargar-todo', (data) => {
+            conexion.recHit(data.database, 'SELECT Codi as id, NOM as nombre, PREU as precioConIva, TipoIva as tipoIva, EsSumable as aPeso FROM Articles').then(res2 => {
+                conexion.recHit(data.database, `SELECT DISTINCT Ambient as nomMenu FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then(res1 => {
+                    if (res1) {
+                        conexion.recHit(data.database, `SELECT Data, Ambient as nomMenu, article as idArticle, pos, color FROM TeclatsTpv WHERE Llicencia = ${data.licencia} AND Data = (select MAX(Data) FROM TeclatsTpv WHERE Llicencia = ${data.licencia} )`).then((res) => {
+                            if (res) {
+                                conexion.recHit(data.database, 'select Codi as idTrabajador, nom as nombre, memo as nombreCorto from dependentes').then(res3 => {
+                                    if (res3) {
                                         let auxObject = {
                                             error: false,
                                             menus: res1.recordset,
@@ -67,21 +59,18 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
                                         };
                                         socket.emit('cargar-todo', auxObject);
                                     }
-                                    else
-                                    {
-                                        socket.emit('cargar-todo', {error: true, infoError: "Error en la respuesta de la consulta SQL 3"});
+                                    else {
+                                        socket.emit('cargar-todo', { error: true, infoError: "Error en la respuesta de la consulta SQL 3" });
                                     }
                                 });
                             }
-                            else
-                            {
-                                socket.emit('cargar-todo', {error: true, infoError: "Error en la respuesta de la consulta SQL"});
+                            else {
+                                socket.emit('cargar-todo', { error: true, infoError: "Error en la respuesta de la consulta SQL" });
                             }
                         });
                     }
-                    else
-                    {
-                        socket.emit('cargar-todo', {error: true, infoError: "Error en la respuesta de la consulta SQL 1"});
+                    else {
+                        socket.emit('cargar-todo', { error: true, infoError: "Error en la respuesta de la consulta SQL 1" });
                     }
                 });
             });
