90d1db14c0e295d6cff13fec583c272b015e678c
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2020-05-06 21:46:12 +0200

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -283,10 +283,10 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
                 `;
 
                 sqlAna = `INSERT INTO feinesafer VALUES (newid(), 'VigilarAlertes', 0, 'Caixa', '[${inicioDay}-${inicioMonth}-${inicioYear} de ${inicioHours}:${inicioMinutes} a ${finalHours}:${finalMinutes}]', '[${data.codigoTienda}]', '${descuadre}', '${sumaEfectivoTarjetaTotal}', getdate());`;
-                sqlAna2 = `insert into feinesafer values (newid(), 'SincroMURANOCaixaOnLine', '[${data.codigoTienda}]', '[${inicioDay}-${inicioMonth}-${inicioYear} ${inicioHours}:${inicioMinutes}:${inicioSeconds}]', ' [${finalDay}-${finalMonth}-${finalYear} ${finalHours}:${finalMinutes}:${finalSeconds}] ', '[${data.arrayCajas[i].primerTicket},${data.arrayCajas[i].ultimoTicket}]', '[${sumaEfectivoTarjetaTotal}]', getdate())  `;
+                sqlAna2 = `insert into feinesafer values (newid(), 'SincroMURANOCaixaOnLine', '[${data.codigoTienda}]', '[${inicioDay}-${inicioMonth}-${inicioYear} ${inicioHours}:${inicioMinutes}:${inicioSeconds}]', ' [${finalDay}-${finalMonth}-${finalYear} ${finalHours}:${finalMinutes}:${finalSeconds}] ', '[${data.arrayCajas[i].primerTicket},${data.arrayCajas[i].ultimoTicket}]', '[${sumaEfectivoTarjetaTotal}]', getdate());`;
 
 
-                let sqlCompleta = sqlZGJ + sqlW + sqlWi + sqlAna;
+                let sqlCompleta = sqlZGJ + sqlW + sqlWi + sqlAna + ;
                 conexion.recHit(data.database, sqlCompleta).then(aux => {
                     socket.emit('confirmarEnvioCaja', {
                         idCaja: data.arrayCajas[i].id,
