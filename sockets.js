1c11510c7197ece7381d1346c02c522cda994fc4
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2020-05-19 00:12:16 +0200

    cesta OK

--- sockets.js
+++ sockets.js
@@ -462,7 +462,7 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
                                                     if (res3) {
                                                         conexion.recHit(data.database, 'SELECT Nom as nombre, Pare as padre FROM Families WHERE Nivell > 0').then(res4 => {
                                                             if (res4) { //Esta consulta debería buscar el codigo de cliente en el paramsHw, según la licencia data.licencia
-                                                                let sqlPromos = `SELECT Id as id, Di as fechaInicio, Df as fechaFinal, D_Producte as principal, D_Quantitat as cantidadPrincipal, S_Producte as secundario, S_Quantitat as cantidadSecundario, S_Preu as precioFinal FROM ProductesPromocionats WHERE Client = ${data.licencia}`;// AND Df > GETDATE()`;
+                                                                let sqlPromos = `SELECT Id as _id, Di as fechaInicio, Df as fechaFinal, D_Producte as principal, D_Quantitat as cantidadPrincipal, S_Producte as secundario, S_Quantitat as cantidadSecundario, S_Preu as precioFinal FROM ProductesPromocionats WHERE Client = ${data.licencia}`;// AND Df > GETDATE()`;
                                                                 conexion.recHit(data.database, sqlPromos).then(res5 => {
                                                                     if (res5) {
                                                                         conexion.recHit(data.database, `select Variable AS nombreDato, Valor AS valorDato from paramsTpv where CodiClient = ${codigoCliente} AND (Variable = 'Capselera_1' OR Variable = 'Capselera_2')`).then(res10 => {
