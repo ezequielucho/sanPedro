cf6dd723b0d40bfd25f2f89db1e4f8d98fd36a5c
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2020-06-19 16:36:35 +0200

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -103,7 +103,7 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
         });
 
         socket.on('comprobarClienteVIP', data=>{
-            conexion.recHit(data.database, `SELECT Codi FROM ConstantsClient WHERE Variable = 'CFINAL' AND Valor = '${data.idCliente}'`).then(res1=>{
+            conexion.recHit(data.database, `SELECT Codi FROM ConstantsClient WHERE Variable = 'CFINAL' AND Valor = '${data.idCliente}';`).then(res1=>{
                 let codiClient = res1.recordset[0].codi;
                 var sql = `
                     IF EXISTS (SELECT * FROM ConstantsClient WHERE Variable = 'EsClient' AND Valor = 'EsClient' AND Codi = ${codiClient})
