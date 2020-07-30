ab19a39ad7535b196438434695bf51e4e54be0a4
Ezequiel <47889917+ezequielucho@users.noreply.github.com>
2020-07-30 12:07:33 +0200

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -554,7 +554,7 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
             try
             {
                 let sql = '';
-
+                let sqlBarras = '';
                 let fecha = new Date(data.info._id);
                 let year = `${fecha.getFullYear()}`;
                 let month = `${fecha.getMonth() + 1}`;
@@ -581,7 +581,10 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
                 let nombreTabla = '[V_Moviments_' + year + '-' + month + ']';
     
                 sql = `INSERT INTO ${nombreTabla} (Botiga, Data, Dependenta, Tipus_moviment, Import, Motiu) VALUES (${data.parametros.codigoTienda}, CONVERT(datetime, '${year}-${month}-${day} ${hours}:${minutes}:${seconds}', 120), ${data.info.idTrabajador}, '${(data.info.tipo == "SALIDA") ? 'O':'A'}', ${data.info.valor}, '${data.info.concepto}');`;
-    
+                if(data.info.tipo == "SALIDA")
+                {
+                    sqlBarras = `INSERT INTO CodisBarresReferencies (Num, Tipus, Estat, Data, TmSt, Param1, Param2, Param3, Param4) VALUES (${data.info.codigoBarras}, 'Moviments', 'Creat', CONVERT(datetime, '${year}-${month}-${day} ${hours}:${minutes}:${seconds}', 120), CONVERT(datetime, '${year}-${month}-${day} ${hours}:${minutes}:${seconds}', 120), ${data.parametros.licencia}, ${data.info.idTrabajador}, ${data.info.valor}, '${day}/${month}/${year} ${hours}:${minutes}:${seconds}')`;
+                }
                 conexion.recHit(data.parametros.database, sql).then(res2 => {
                     socket.emit('confirmarEnvioMovimiento', {
                         idMovimiento: data.info._id,
