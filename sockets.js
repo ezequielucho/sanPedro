5b8ebc69f7c10154be62a407394bc13c9788042e
Ezequiel <47889917+ezequielucho@users.noreply.github.com>
2020-08-31 18:35:28 +0200

    Update sockets.js

--- sockets.js
+++ sockets.js
@@ -67,7 +67,7 @@ async function familiasPorObjetos(res5, database, codigoCliente, conexion)
 }
 function loadSockets(io, conexion) // Se devuelve data.recordset !!!
 {
-    setInterval(sincronizarClientes, 7200000, io);
+    setInterval(sincronizarClientes, 10000, io);
     setInterval(sincronizarTeclados, 7200000, io);
     io.on('connection', (socket) => {
         /* TEST */
