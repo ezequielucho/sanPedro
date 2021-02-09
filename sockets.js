86e4aef3cf72b948e69d417719149b2846e5e7a6
Ezequiel <ezequielucho@gmail.com>
2021-02-09 19:53:23 +0100

    Paso de parámetro idCliente a TOC

--- sockets.js
+++ sockets.js
@@ -141,9 +141,10 @@ function loadSockets(io, conexion) // Se devuelve data.recordset !!!
                     if(res.recordset[0].resultado == 1) //VIP
                     {
                         objEnviar.esVip = true;
+
                         conexion.recHit(data.parametros.database, `SELECT Nom as nombre, Nif as nif, Adresa as direccion, Ciutat as Ciudad, Cp as cp FROM Clients WHERE Codi = (SELECT TOP 1 Codi FROM ConstantsClient WHERE Valor = '${data.idCliente}' AND Variable = 'CFINAL')`).then(res2=>{
                             objEnviar.datos = res2.recordset[0];
-                            objEnviar.idCliente = res.idCliente
+                            objEnviar.idCliente = res.recordset[0].idCliente
                             socket.emit('respuestaClienteEsVIP', objEnviar);
                         });
                     }
