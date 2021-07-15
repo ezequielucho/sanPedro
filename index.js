c8d4fcf44743f2da8f20de5a0dc4e0b4fab02a28
Eze <ezequielucho@gmail.com>
2021-07-15 10:54:49 +0200

    Glovo Webhook added

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ app.set('port', process.env.PORT || 8080);
 app.set('Set-Cookie', 'HttpOnly;Secure;SameSite=Strict');
 //STATIC FILES
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.post("/creation", (req, res) => {
+  res.json({tipo: 'creation'});
+});
+app.post("/cancellation", (req, res) => {
+  res.json({tipo: 'cancellation'});
+});
 //START SERVER
 const server = app.listen(app.get('port'), () =>{
     console.log('server on port', app.get('port'));
